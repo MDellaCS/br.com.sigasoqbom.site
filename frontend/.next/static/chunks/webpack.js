9a06891939c4b3fe80394391b52f11ad11491bf1
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-09 20:32:50 -0300

    removed fonts fallback

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a5839fcf63c4944"; }
+/******/ 		__webpack_require__.h = function() { return "a3d4f330194e55e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
