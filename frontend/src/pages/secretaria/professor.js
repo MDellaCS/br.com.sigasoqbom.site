1b03038862712ce05b1b5bb499ee5ddf2201b154
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-09 15:44:30 -0300

    Cadastro professor

--- frontend/src/pages/secretaria/professor.js
+++ frontend/src/pages/secretaria/professor.js
@@ -20,39 +20,16 @@ export default function Home() {
 
         <div className={css.row}>
           <Input id="nome" title="Nome Completo" type="text" />
+        </div>
+
+        <div className={css.row}>
+          <Input id="email" title="Email Corporativo" type="email" />
+        </div>
+
+        <div className={css.row}>
           <Input id="nascimento" title="Data de Nascimento" type="date" />
-        </div>
-
-        <div className={css.row}>
-          <Input id="nomesocial" title="Nome Social" type="text" />
-        </div>
-
-        <div className={css.row}>
-          <Input id="ra" title="Registro Acadêmico" type="text" />
           <Input id="cpf" title="CPF" type="text" />
         </div>
-
-        <div className={css.row}>
-          <Input id="email" title="Email Pessoal" type="email" />
-        </div>
-
-        <div className={css.row}>
-          <Input id="instituicao" title="Instituição de Conclusão do 2º Grau" type="text" />
-          <Input id="conclusao" title="Ano de Conclusão" type="number" />
-        </div>
-
-        <div className={css.row}>
-          <Input id="pontuacao" title="Pontuação" type="number" />
-          <Input id="posicao" title="Posição" type="number" />
-        </div>
-
-        <label for="curso" className={css.lbl}>Curso</label>
-        <br />
-        <select id="curso" className={css.formInput} name="cursoAluno" required>
-          <option value="" selected disabled hidden>Escolha o Curso</option>
-          {/* Pegar os cursos do BD */}
-        </select>
-        <br />
 
         <div className={css.center}>
           <Button href=" " value="Inserir" />
