efa845cc85af8a070b4958945a8fdc7aca547f2d
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-11 10:52:00 -0300

    cmt

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bf65139cff9330c9"; }
+/******/ 		__webpack_require__.h = function() { return "9141d895e78645f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
