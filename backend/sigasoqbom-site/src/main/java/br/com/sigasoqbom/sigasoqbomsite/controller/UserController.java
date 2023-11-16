07d695a7e56ec2bd856b7c18882c7a7d68ca7c59
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-16 15:29:40 -0300

    Aprendendo API

--- backend/sigasoqbom-site/src/main/java/br/com/sigasoqbom/sigasoqbomsite/controller/UserController.java
+++ backend/sigasoqbom-site/src/main/java/br/com/sigasoqbom/sigasoqbomsite/controller/UserController.java
@@ -1,45 +1,33 @@
 package br.com.sigasoqbom.sigasoqbomsite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sigasoqbom.sigasoqbomsite.model.User;
+import br.com.sigasoqbom.sigasoqbomsite.repository.UserRepository;
+
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("user")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+   @Autowired
+   private UserRepository userRepository;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+   @GetMapping
+   public ResponseEntity<String> getAll() {
+      return ResponseEntity.status(HttpStatus.OK).body("Alo User API");
+   }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        Optional<User> user = userRepository.findById(id);
-        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
+   @PostMapping(consumes = "application/json")
+   public ResponseEntity<User> createUser(@RequestBody User user) {
+      User savedUser = userRepository.save(user);
+      return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+   }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
-        if (!userRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        user.setId(id);
-        User updatedUser = userRepository.save(user);
-        return ResponseEntity.ok(updatedUser);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        if (!userRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        userRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
 }
--- backend/sigasoqbom-site/src/main/java/br/com/sigasoqbom/sigasoqbomsite/repository/UserRepository.java
+++ backend/sigasoqbom-site/src/main/java/br/com/sigasoqbom/sigasoqbomsite/repository/UserRepository.java
@@ -1,30 +1,10 @@
 package br.com.sigasoqbom.sigasoqbomsite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import br.com.sigasoqbom.sigasoqbomsite.model.User;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    User save(User user);
-
-    Optional<User> findById(Long id);
-
-    List<User> findAll();
-
-    long count();
-
-    void deleteById(Long id);
-
-    void delete(User user);
-
-    boolean existsById(Long id);
-
-    boolean existsAllByIdIn(Iterable<Long> ids);
-
-    Optional<User> findByUsername(String username);
-
 }
