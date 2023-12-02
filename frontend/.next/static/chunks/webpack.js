65250c10ae67bd6fb2f8c208628c581f0e6bc5e2
Matheus Della <matheus.com>
2023-12-02 10:51:43 -0300

    commit

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d5d139006de1214d"; }
+/******/ 		__webpack_require__.h = function() { return "193e750a718cbd72"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
